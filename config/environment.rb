c442e8c70f72d51e32d0ca3149f400234456ee02
J-Agens <johntagens@gmail.com>
2020-01-26 21:53:07 -0500

    datadog: more stuff

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,13 @@ require 'tty-prompt'
 ############# ADDED FOR DATADOG EXERCISE ####################
 require 'datadog/statsd'
 statsd = Datadog::Statsd.new('localhost', 8125)
+
+while true do
+    statsd.increment('example_metric.increment', tags: ['environment:dev'])
+    statsd.decrement('example_metric.decrement', tags: ['environment:dev'])
+    statsd.count('example_metric.count', 2, tags: ['environment:dev'])
+    sleep 10
+end
 ##############################################################
 Bundler.require
 
